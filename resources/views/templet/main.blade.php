3a6cf29f6ab046fb3b52951bce5c514ac0664a20
Rayuzan-Mojavec <rayuzanmojavec@gmail.com>
2024-11-14 22:13:05 +0700

    moving to templet

---
+++ resources/views/templet/main.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>THT 18123011</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:500" rel="stylesheet" />
+
+    <!-- Styles / Scripts -->
+    <link rel="icon" type="image/png" href="{{ asset('imt.png') }}">
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+        <script src="https://cdn.tailwindcss.com"></script>
+    @endif
+</head>
+
+<body class="flex gap-5 flex-col p-20 justify-center content-center text-center items-center bg-gradient-to-r from-[#2C4981] to-[#d0d0d0]">
+    @yield('content_room')
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,90 +1,69 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('templet.main')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>THT 18123011</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:500" rel="stylesheet" />
-
-    <!-- Styles / Scripts -->
-    <link rel="icon" type="image/png" href="{{ asset('imt.png') }}">
-    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @else
-        <script src="https://cdn.tailwindcss.com"></script>
-    @endif
-</head>
-
-<body
-    class="flex gap-5 flex-col p-20 justify-center content-center text-center items-center bg-gradient-to-r from-[#2C4981] to-[#d0d0d0]">
-    <div class="bg-slate-100/20 inline-flex w-full p-10 rounded-lg drop-shadow text-xl gap-x-5">
-        <img src="{{ asset('34242.jpg') }}" alt="" class="rounded-md w-1/4 h-min">
-        <div class="w-3/4  text-justify ">
-            <p>
-                Ini sih mudah ya, foto ama bang Nabil abis lantik. Bagaikan hadiah setelah diuji otak selama 5 hari.
-                Udah
-                itu aja sih, kalau day mah tiap day ada aja ceritanya sendiri.
-                Abis ini ya paling beban akademik kek biasa, tapi prak RL sih selalu gacor, hmmm apalagi ya
-                Ya palingan hidup di sekre itu lumayan juga, bisa jajan terus tuh ampe blastoz cepet abis mulu
-                awokwokwokwok menunggu pak munir ga dateng tapi dateng pas sebelum 20 menit lewat :(( nilai probstat
-                lebih jelek dari PSWK, kaget ya sama kaget juga
-            </p>
-            <br>
-            <p>
-                Rasanya sih ya paling berusaha hidup sebahagia mungkin (boong) di kampus ini, setidaknya daripada
-                nganggur ga jelas di rumah, tapi ya beratttttt
-            </p>
-            <br>
-            <p>
-                Imagine being able to confidently give your answer in front of your friends and teacher and it turns out
-                to be correct, sungguh sigma
-            </p>
-        </div>
-    </div>
-    <div class="bg-slate-100/20 inline-grid w-full my-auto p-10 gap-y-5 rounded-lg drop-shadow text-xl">
-        <h1 class="text-center text-5xl font-bold">Mantra HTML</h1>
-        <p class="text-center font-semibold">Konon membaca ini setiap akan tidur adalah buang-buang waktu</p>
-        <p class="text-justify">
-            Lorem ipsum odor amet, consectetuer adipiscing elit. Sit in maecenas, quam in rhoncus consectetur maximus
-            imperdiet. Eros vitae quam per libero morbi curabitur dui tristique. Nibh sem euismod phasellus tincidunt
-            ullamcorper. Ut fames tincidunt luctus ullamcorper curabitur vitae justo tortor libero. Gravida potenti
-            lectus
-            bibendum odio primis. Nulla vel est fusce magna varius tristique risus hendrerit. Taciti nam himenaeos
-            interdum
-            dis semper magnis. Fames potenti placerat penatibus pretium mauris rhoncus turpis risus cras. Aliquam orci
-            bibendum malesuada orci odio consectetur vivamus tellus.
-
-            Metus non porttitor sodales vivamus phasellus bibendum pulvinar. Curae molestie commodo ex habitasse ligula
-            ipsum fermentum vulputate? Praesent magna nisl conubia pellentesque nunc platea. Imperdiet sagittis ex
-            dignissim
-            tortor elit turpis torquent natoque. Ex consequat elementum fusce nulla gravida faucibus? Tellus facilisi
-            mollis
-            nisl dui sit malesuada. Magna rhoncus metus sed habitasse leo vel senectus nibh. Malesuada efficitur lectus
-            cursus mauris et eu sociosqu.
-
-            Lorem ipsum odor amet, consectetuer adipiscing elit. Sit in maecenas, quam in rhoncus consectetur maximus
-            imperdiet. Eros vitae quam per libero morbi curabitur dui tristique. Nibh sem euismod phasellus tincidunt
-            ullamcorper. Ut fames tincidunt luctus ullamcorper curabitur vitae justo tortor libero. Gravida potenti
-            lectus
-            bibendum odio primis. Nulla vel est fusce magna varius tristique risus hendrerit. Taciti nam himenaeos
-            interdum
-            dis semper magnis. Fames potenti placerat penatibus pretium mauris rhoncus turpis risus cras. Aliquam orci
-            bibendum malesuada orci odio consectetur vivamus tellus.
-
-            Metus non porttitor sodales vivamus phasellus bibendum pulvinar. Curae molestie commodo ex habitasse ligula
-            ipsum fermentum vulputate? Praesent magna nisl conubia pellentesque nunc platea. Imperdiet sagittis ex
-            dignissim
-            tortor elit turpis torquent natoque. Ex consequat elementum fusce nulla gravida faucibus? Tellus facilisi
-            mollis
-            nisl dui sit malesuada. Magna rhoncus metus sed habitasse leo vel senectus nibh. Malesuada efficitur lectus
-            cursus mauris et eu sociosqu.
+@section('content_room')
+    
+<div class="bg-slate-100/20 inline-flex w-full p-10 rounded-lg drop-shadow text-xl gap-x-5">
+    <img src="{{ asset('34242.jpg') }}" alt="" class="rounded-md w-1/4 h-min">
+    <div class="w-3/4  text-justify ">
+        <p>
+            Ini sih mudah ya, foto ama bang Nabil abis lantik. Bagaikan hadiah setelah diuji otak selama 5 hari.
+            Udah
+            itu aja sih, kalau day mah tiap day ada aja ceritanya sendiri.
+            Abis ini ya paling beban akademik kek biasa, tapi prak RL sih selalu gacor, hmmm apalagi ya
+            Ya palingan hidup di sekre itu lumayan juga, bisa jajan terus tuh ampe blastoz cepet abis mulu
+            awokwokwokwok menunggu pak munir ga dateng tapi dateng pas sebelum 20 menit lewat :(( nilai probstat
+            lebih jelek dari PSWK, kaget ya sama kaget juga
+        </p>
+        <br>
+        <p>
+            Rasanya sih ya paling berusaha hidup sebahagia mungkin (boong) di kampus ini, setidaknya daripada
+            nganggur ga jelas di rumah, tapi ya beratttttt
+        </p>
+        <br>
+        <p>
+            Imagine being able to confidently give your answer in front of your friends and teacher and it turns out
+            to be correct, sungguh sigma
         </p>
     </div>
-</body>
+</div>
+<div class="bg-slate-100/20 inline-grid w-full my-auto p-10 gap-y-5 rounded-lg drop-shadow text-xl">
+    <h1 class="text-center text-5xl font-bold">Mantra HTML</h1>
+    <p class="text-center font-semibold">Konon membaca ini setiap akan tidur adalah buang-buang waktu</p>
+    <p class="text-justify">
+        Lorem ipsum odor amet, consectetuer adipiscing elit. Sit in maecenas, quam in rhoncus consectetur maximus
+        imperdiet. Eros vitae quam per libero morbi curabitur dui tristique. Nibh sem euismod phasellus tincidunt
+        ullamcorper. Ut fames tincidunt luctus ullamcorper curabitur vitae justo tortor libero. Gravida potenti
+        lectus
+        bibendum odio primis. Nulla vel est fusce magna varius tristique risus hendrerit. Taciti nam himenaeos
+        interdum
+        dis semper magnis. Fames potenti placerat penatibus pretium mauris rhoncus turpis risus cras. Aliquam orci
+        bibendum malesuada orci odio consectetur vivamus tellus.
 
-</html>
+        Metus non porttitor sodales vivamus phasellus bibendum pulvinar. Curae molestie commodo ex habitasse ligula
+        ipsum fermentum vulputate? Praesent magna nisl conubia pellentesque nunc platea. Imperdiet sagittis ex
+        dignissim
+        tortor elit turpis torquent natoque. Ex consequat elementum fusce nulla gravida faucibus? Tellus facilisi
+        mollis
+        nisl dui sit malesuada. Magna rhoncus metus sed habitasse leo vel senectus nibh. Malesuada efficitur lectus
+        cursus mauris et eu sociosqu.
+
+        Lorem ipsum odor amet, consectetuer adipiscing elit. Sit in maecenas, quam in rhoncus consectetur maximus
+        imperdiet. Eros vitae quam per libero morbi curabitur dui tristique. Nibh sem euismod phasellus tincidunt
+        ullamcorper. Ut fames tincidunt luctus ullamcorper curabitur vitae justo tortor libero. Gravida potenti
+        lectus
+        bibendum odio primis. Nulla vel est fusce magna varius tristique risus hendrerit. Taciti nam himenaeos
+        interdum
+        dis semper magnis. Fames potenti placerat penatibus pretium mauris rhoncus turpis risus cras. Aliquam orci
+        bibendum malesuada orci odio consectetur vivamus tellus.
+
+        Metus non porttitor sodales vivamus phasellus bibendum pulvinar. Curae molestie commodo ex habitasse ligula
+        ipsum fermentum vulputate? Praesent magna nisl conubia pellentesque nunc platea. Imperdiet sagittis ex
+        dignissim
+        tortor elit turpis torquent natoque. Ex consequat elementum fusce nulla gravida faucibus? Tellus facilisi
+        mollis
+        nisl dui sit malesuada. Magna rhoncus metus sed habitasse leo vel senectus nibh. Malesuada efficitur lectus
+        cursus mauris et eu sociosqu.
+    </p>
+</div>
+
+@endsection
