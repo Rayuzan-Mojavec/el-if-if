7561e67bb3bd03d5e19266ed6f1c3b00c884e730
Rayuzan-Mojavec <rayuzanmojavec@gmail.com>
2024-11-14 21:53:59 +0700

    added my story

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,24 +12,43 @@
     <link href="https://fonts.bunny.net/css?family=instrument-sans:500" rel="stylesheet" />
 
     <!-- Styles / Scripts -->
-    <link rel="icon" type="image/png" href="{{ asset('ext.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('imt.png') }}">
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     @else
-
+        <script src="https://cdn.tailwindcss.com"></script>
     @endif
 </head>
 
 <body
     class="flex gap-5 flex-col p-20 justify-center content-center text-center items-center bg-gradient-to-r from-[#2C4981] to-[#d0d0d0]">
-    <div class="bg-slate-100/20 inline-flex w-full p-10 rounded-lg drop-shadow text-xl text-justify gap-x-5">
-        <img src="{{ asset('ext.png') }}" alt="" class="rounded-full w-1/5 h-min">
-        <div class="w-4/5">
-           Kalau ini gimana ya, susah, semuanya menarik, kecuali forum
+    <div class="bg-slate-100/20 inline-flex w-full p-10 rounded-lg drop-shadow text-xl gap-x-5">
+        <img src="{{ asset('34242.jpg') }}" alt="" class="rounded-md w-1/4 h-min">
+        <div class="w-3/4  text-justify ">
+            <p>
+                Ini sih mudah ya, foto ama bang Nabil abis lantik. Bagaikan hadiah setelah diuji otak selama 5 hari.
+                Udah
+                itu aja sih, kalau day mah tiap day ada aja ceritanya sendiri.
+                Abis ini ya paling beban akademik kek biasa, tapi prak RL sih selalu gacor, hmmm apalagi ya
+                Ya palingan hidup di sekre itu lumayan juga, bisa jajan terus tuh ampe blastoz cepet abis mulu
+                awokwokwokwok menunggu pak munir ga dateng tapi dateng pas sebelum 20 menit lewat :(( nilai probstat
+                lebih jelek dari PSWK, kaget ya sama kaget juga
+            </p>
+            <br>
+            <p>
+                Rasanya sih ya paling berusaha hidup sebahagia mungkin (boong) di kampus ini, setidaknya daripada
+                nganggur ga jelas di rumah, tapi ya beratttttt
+            </p>
+            <br>
+            <p>
+                Imagine being able to confidently give your answer in front of your friends and teacher and it turns out
+                to be correct, sungguh sigma
+            </p>
         </div>
     </div>
-    <div class="bg-slate-100/20 flex w-full my-auto p-10 rounded-lg drop-shadow text-xl text-justify">
-        <div>
+    <div class="bg-slate-100/20 inline-grid w-full my-auto p-10 gap-y-5 rounded-lg drop-shadow text-xl">
+        <h1 class="text-center text-5xl font-bold">Mantra HTML</h1>
+        <p class="text-justify">
             Lorem ipsum odor amet, consectetuer adipiscing elit. Sit in maecenas, quam in rhoncus consectetur maximus
             imperdiet. Eros vitae quam per libero morbi curabitur dui tristique. Nibh sem euismod phasellus tincidunt
             ullamcorper. Ut fames tincidunt luctus ullamcorper curabitur vitae justo tortor libero. Gravida potenti
@@ -63,7 +82,7 @@
             mollis
             nisl dui sit malesuada. Magna rhoncus metus sed habitasse leo vel senectus nibh. Malesuada efficitur lectus
             cursus mauris et eu sociosqu.
-        </div>
+        </p>
     </div>
 </body>
 
