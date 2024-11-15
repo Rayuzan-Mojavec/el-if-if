f9c2dee4364223dd14df45d1b1d85b5c041c22ec
Rayuzan-Mojavec <rayuzanmojavec@gmail.com>
2024-11-15 11:13:00 +0700

    components beibeh

--- resources/views/templet/navbar.blade.php
+++ resources/views/templet/navbar.blade.php
@@ -1,12 +1,12 @@
-<nav class="bg-slate-100/80 flex items-center p-5 text-black text-center rounded-b-xl w-full">
-    <ul class="flex flex-row items-center gap-2">
-        <li>
+<nav class="bg-slate-100/80 flex items-center p-5 text-white text-center rounded-b-xl w-full">
+    <ul class="flex flex-row items-center gap-10 text-lg">
+        <li class="bg-[#2C4981] px-3 py-1 rounded-md shadow-md">
             Halo
         </li>
-        <li>
+        <li class="bg-[#2C4981] px-3 py-1 rounded-md shadow-md">
             Halo
         </li>
-        <li>
+        <li class="bg-[#2C4981] px-3 py-1 rounded-md shadow-md">
             Halo
         </li>
     </ul>
