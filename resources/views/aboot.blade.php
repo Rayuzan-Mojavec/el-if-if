674ac4712524f8ed27855c7ffdf19dda0008922c
Rayuzan-Mojavec <rayuzanmojavec@gmail.com>
2024-11-15 22:05:27 +0700

    footer active link

---
+++ resources/views/aboot.blade.php
@@ -0,0 +1,15 @@
+@extends('templet.main')
+
+@section('content_room')
+    
+<div class="content_card flex flex-col gap-y-5 justify-center items-center">
+    <h1 class="font-bold text-5xl">
+        Anak kesayangan mas rusdi
+    </h1>
+    <div class="w-1/3">
+        <img src="{{ asset('87123.png') }}" class="rounded-md shadow-xl" alt="Harusnya ada hiura di sini">
+    </div>
+</div>
+
+
+@endsection
