a6ed8bac41def8008b27345bf5bf0a16b49119b7
Rayuzan-Mojavec <rayuzanmojavec@gmail.com>
2024-11-15 13:09:49 +0700

    navbar simplification

--- resources/views/templet/navbar.blade.php
+++ resources/views/templet/navbar.blade.php
@@ -1,13 +1,9 @@
 <nav class="bg-slate-100/80 flex items-center p-5 text-white text-center rounded-b-xl w-full mb-14">
     <ul class="flex flex-row items-center gap-10 text-lg">
         <li class="bg-[#2C4981] px-3 py-1 rounded-md shadow-md">
-            Halo
-        </li>
-        <li class="bg-[#2C4981] px-3 py-1 rounded-md shadow-md">
-            Halo
-        </li>
-        <li class="bg-[#2C4981] px-3 py-1 rounded-md shadow-md">
-            Halo
+            <a href="/">
+                Halo
+            </a>
         </li>
     </ul>
 </nav>
