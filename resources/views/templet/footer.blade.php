7059f4a07fcf5d3e480a05609bd0f942a321a499
Rayuzan-Mojavec <rayuzanmojavec@gmail.com>
2024-11-15 17:21:49 +0700

    footer complete

--- resources/views/templet/footer.blade.php
+++ resources/views/templet/footer.blade.php
@@ -1,3 +1,47 @@
-<footer class="bg-slate-100/50 w-full p-5 mt-14">
-    idk
-</footer> 
+<div class="bg-slate-100/50 w-full flex flex-col items-center gap-y-4 p-5 mt-14">
+    <ul class="inline-flex gap-x-6">
+        <li>
+            <a href="https://x.com/Rayuzan_M" class="social_icons">
+                <i class="bi-twitter-x"></i>
+            </a>
+        </li>
+        <li>
+            <a href="https://www.facebook.com/profile.php?id=100010368942719" class="social_icons">
+                <i class="bi-facebook"></i>
+            </a>
+        </li>
+        <li>
+            <a href="https://www.instagram.com/rayuzan_m/" class="social_icons">
+                <i class="bi-instagram"></i>
+            </a>
+        </li>
+        <li>
+            <a href="https://github.com/Rayuzan-Mojavec" class="social_icons">
+                <i class="bi-github"></i>
+            </a>
+        </li>
+        <li>
+            <a href="https://www.reddit.com/user/Rayuzan_Mojavec/" class="social_icons">
+                <i class="bi-reddit"></i>
+            </a>
+        </li>
+        <li>
+            <a href="https://open.spotify.com/user/21xhs5z55jc2tgrqrvxnz4kji" class="social_icons">
+                <i class="bi-spotify"></i>
+            </a>
+        </li>
+        <li>
+            <a href="https://steamcommunity.com/profiles/76561198828589517/" class="social_icons">
+                <i class="bi-steam"></i>
+            </a>
+        </li>
+        <li>
+            <a href="https://www.youtube.com/@Rayuzan_M" class="social_icons">
+                <i class="bi-youtube"></i>
+            </a>
+        </li>
+    </ul>
+    <div class="flex justify-center text-lg">
+        Copyright 2024 PT Foreas. All Rights Reserved
+    </div>
+</div>
